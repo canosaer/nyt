6d0a7cf1c2fa77b48dc1029b593d27b22ccf1b03
canosaer <canosaer@gmail.com>
2021-04-12 15:17:45 -0400

    after class

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,4 +1,2 @@
-"use strict";
-
-console.log("Hello World from main.js! \nChange this message, and make sure it changes in the browser \nto verify that you're working in the right files.");
+class NYT_SearchAPI{API_BASE_URL="https://api.nytimes.com/svc/search/v2/articlesearch.json";API_KEY="A1AMntlR77BOSIKIhTCl7YtvA3MjAW3U";constructor(){this.setupListener()}setupListener(){document.querySelector('form[name="article-search"]').addEventListener("submit",this.handleSearch)}handleSearch=e=>{e.preventDefault(),console.log("searching...");const s={q:document.querySelector('input[name="term"]'),"api-key":this.API_KEY};axios.get(this.API_BASE_URL,{params:s}).then(this.processResults)};processResults=e=>{console.log("got results!",e)}}new NYT_SearchAPI;
 //# sourceMappingURL=main.js.map
--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,37 @@
-console.log(`Hello World from main.js! 
-Change this message, and make sure it changes in the browser 
-to verify that you're working in the right files.`);
+class NYT_SearchAPI {
+    API_BASE_URL = `https://api.nytimes.com/svc/search/v2/articlesearch.json`
+    API_KEY = `A1AMntlR77BOSIKIhTCl7YtvA3MjAW3U`
+
+    constructor() {
+        this.setupListener()
+    }
+
+    setupListener() {
+        const form = document.querySelector(`form[name="article-search"]`)
+        form.addEventListener(`submit`, this.handleSearch)
+    }
+
+    handleSearch = (evt) => {
+        evt.preventDefault()
+        console.log(`searching...`)
+
+        const term = document.querySelector(`input[name="term"]`)
+        const data = {
+            q: term,
+            'api-key': this.API_KEY
+        }
+
+        axios.get( this.API_BASE_URL, { params: data }).then(this.processResults)
+    }
+
+    processResults = (response) => {
+        console.log(`got results!`, response)
+
+        // const results = response.data.response.docs
+        // results.array.forEach(element => {
+            
+        // });
+    }
+}
+
+new NYT_SearchAPI
